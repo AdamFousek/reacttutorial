45b04d78a5a3c5ba80c6e552bef077e6d0b1b722
AdamFousek <adam-fousek@seznam.cz>
2021-07-07 13:42:07 +0200

    Section 2 Complete
    Working with events, two way bindings, communicate parent child comp

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,16 +1,27 @@
+import { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem.js";
+import ExpenseFilter from "./ExpenseFilter.js";
 import Card from '../UI/Card';
 
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const setFilterYearHandler = (enteredYearData) => {
+    setFilteredYear(enteredYearData);
+    console.log(enteredYearData)
+  }
   return (
-    <Card className="expenses">
-      <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-      <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-      <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-      <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter year={filteredYear} onSetFilterYear={setFilterYearHandler} />
+        <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+        <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
+        <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
+        <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+      </Card>
+    </div>
   );
 }
 
