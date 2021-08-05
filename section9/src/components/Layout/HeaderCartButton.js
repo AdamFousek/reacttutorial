981c6a4022584a247e71c1ac7c9d6413d6a9c3aa
AdamFousek <adam-fousek@seznam.cz>
2021-08-05 19:19:45 +0200

    Section 9 Complete
    Full page with ordering

--- section9/src/components/Layout/HeaderCartButton.js
+++ section9/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,45 @@
+import { useContext, useEffect, useState } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-    return <button className={classes.button}>
+    const [buttonAnimation, setButtonAnimation] = useState(false);
+    const cartCtx = useContext(CartContext);
+
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+
+    const btnClasses = `${classes.button} ${buttonAnimation ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setButtonAnimation(true);
+
+        const timer = setTimeout(() => {
+            setButtonAnimation(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your cart</span>
         <span className={classes.badge}>
-            3
+            {numberOfCartItems}
         </span>
     </button>
 }
--- section9/src/components/Meals/MealItem/MealItemForm.js
+++ section9/src/components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,24 @@
+import { useRef, useState } from 'react';
+
 import Input from '../../UI/Input';
 
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+    const submiteHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false);
+            return
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    };
     const input = {
         id: 'amount_' + props.id,
         type: 'number',
@@ -11,9 +27,10 @@ const MealItemForm = (props) => {
         step: '1',
         defaultValue: '1'
     }
-    return <form className={classes.form}>
-        <Input label={"Amount"} input={input} />
+    return <form className={classes.form} onSubmit={submiteHandler}>
+        <Input label={"Amount"} input={input} ref={amountInputRef} />
         <button>+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount</p>}
     </form>;
 }
 
