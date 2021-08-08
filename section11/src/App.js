c7bc99ab4003941ab20f22ce5184eab43a9048bc
AdamFousek <adam-fousek@seznam.cz>
2021-08-08 17:34:26 +0200

    WIP: Section 11
    Http requests

---
+++ section11/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    const transformMovies = data.results.map(item => {
+      return {
+        id: item.episode_id,
+        title: item.title,
+        releaseDate: item.release_date,
+        openingText: item.opening_crawl
+      }
+    })
+    setMovies(transformMovies);
+    setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies!</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
