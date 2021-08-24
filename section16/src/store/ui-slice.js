5a996f649240155e4a2a3762b8b3c641676f4e0b
AdamFousek <adam-fousek@seznam.cz>
2021-08-24 21:09:10 +0200

    WIP: Section 16 http requests
    redux-toolkit and http requests inside of components

--- section16/src/store/ui-slice.js
+++ section16/src/store/ui-slice.js
@@ -4,11 +4,15 @@ const uiSlice = createSlice({
     name: 'ui',
     initialState: {
         cartIsVisible: false,
+        notification: null,
     },
     reducers: {
         toggle(state) {
             state.cartIsVisible = !state.cartIsVisible;
         },
+        showNotification(state, action) {
+            state.notification = action.payload;
+        }
     }
 });
 
