3bc1dae218f7553dba9bf695105556d63523caf6
AdamFousek <adam-fousek@seznam.cz>
2021-08-24 21:54:43 +0200

    Section 16 Complete
    Actions and dispatching reducers for data

--- section16/src/App.js
+++ section16/src/App.js
@@ -4,7 +4,8 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
 
@@ -15,45 +16,12 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      if (isInitial) {
-        isInitial = false;
-        return
-      }
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data to server'
-      }));
-      const response = await fetch('https://react-test-58ecf-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        });
-      if (!response.ok) {
-        throw new Error('Something went wrong');
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success',
-        message: 'Send cart data successfully!'
-      }));
-    }
-
     if (isInitial) {
+      dispatch(fetchCartData());
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error',
-        message: error.message
-      }));
-    })
-
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
